204fcbfd6ab6e9d97b6f25840cce7bc899a7d4f8
Parker Smart <psmart@spilledmilk.com>
2017-12-06 07:17:48 -0700

    Adding placeholder projects.

---
+++ SM.UWP.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.UWP.Common
+{
+    public class Class1
+    {
+    }
+}
